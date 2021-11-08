448da587d7fcaac046126b15653a05fdab008636
Palilu <palilu@icloud.com>
2021-11-08 17:59:41 -0300

    Agrego un poco de código al archivo javascript por las dudas.

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let integrantes = ["Agustín", "Guillermina", "Juliana", "Matías", "Pablo", "Ricardo", "Selena", "Yanela"];
+
+for (let i = 0; i < integrantes.length; i++) {
+    console.log(integrantes[i]);
+}
